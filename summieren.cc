ef869fbeb48004c5edaa616120e97f7aedff1d1c
repl.it user <replituser@example.com>
2020-11-04 14:43:17 +0000

    Augabe 2

---
+++ summieren.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+  std::ifstream infile;
+  infile.open("daten.txt");
+  std::ofstream outfile("datensumme.txt");
+  int a,b;
+  while(! infile.eof()){
+    infile >> a >> b;
+    outfile << a+b << std::endl;
+  }  
+
+  infile.close();
+  outfile.close();
+}
+
